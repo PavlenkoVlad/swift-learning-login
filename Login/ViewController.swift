54f057b5c504b44c6aa8ca7f67105f30093c0819
Vlad Pavlenko <pavlenko0902@gmail.com>
2019-06-29 20:26:42 +0300

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Владислав Павленко on 6/29/19.
+//  Copyright © 2019 Владислав Павленко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
